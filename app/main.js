72cfcbb443ed1b6787f0b5ded30ec5775dadaec2
kkrisyo <42550275+kkrisyo@users.noreply.github.com>
2018-08-25 01:15:41 +0200

    on

--- app/main.js
+++ app/main.js
@@ -135,4 +135,4 @@ app.on('ready', () => {
 
 const Gpio = require('onoff').Gpio;
 const button = new Gpio(4, 'in', 'both');
-button.watch((err, value) => console.log(value));
+button.watch((err, value) => mainWindow.webContents.send('ping', val) );
